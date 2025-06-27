bd065d3f01562b1bd42b047e7fbf576efbdca4c5
Thanos Tsiamis <thanos.tsiamis@allwyn-lotterysolutions.com>
2025-06-27 11:45:50 +0300

    Add MIT license

--- bias_detector/scanner.py
+++ bias_detector/scanner.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Agile Actors
+# Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
 import re
 import glob
 from tqdm import tqdm
