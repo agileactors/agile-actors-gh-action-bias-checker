a4b042ff67ad7f48d3a4fb7b83cbec6d519f73c2
Thanos Tsiamis <thanos.tsiamis@allwyn-lotterysolutions.com>
2025-07-16 17:21:57 +0300

    feat: Add GitHub Actions workflow for gender bias scanning

---
+++ demo-proj/testFile.py
@@ -0,0 +1,12 @@
+barwoman = "barwoman"
+def bar():
+    return "bar"
+def foo():
+    return "foo"
+barman = "barman"
+
+def baz():
+    return "I am a strong man and I can do anything"
+hello = "hello"
+def world():
+    return "world"
